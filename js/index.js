0e88d25a646e4ef519602fb7b396c4d806f4a7e5
Mathieu Pillard <m@virgule.net>
2015-08-28 15:05:16 +0200

    Convert to Web Extensions format

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-window.addEventListener('DOMContentLoaded', function() {
+// If injecting into an app that was already running at the time
+// the app was enabled, simply initialize it.
+if (document.documentElement) {
+  initialize();
+}
+// Otherwise, we need to wait for the DOM to be ready before
+// starting initialization since add-ons are usually (always?)
+// injected *before* `document.documentElement` is defined.
+else {
+  window.addEventListener('DOMContentLoaded', initialize);
+}
+
+function initialize() {
   alert('Hello World!');
-});
+}
